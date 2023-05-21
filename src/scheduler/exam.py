c5c9be48d1164fe82aa27d6ad4bb6801b56e0e4d
oedokumaci <oral.ersoy.dokumaci@gmail.com>
2023-05-21 17:28:53 +0300

    refactor: add docstrings and comments

--- src/scheduler/exam.py
+++ src/scheduler/exam.py
@@ -7,6 +7,15 @@ class Exam:
     def __init__(
         self, title: str, date: str, time: str, classroom: str, instructor: str
     ) -> None:
+        """Initialize an Exam object.
+
+        Args:
+            title (str): The title of the exam.
+            date (str): The date of the exam.
+            time (str): The time of the exam.
+            classroom (str): The classroom for the exam.
+            instructor (str): The instructor for the exam.
+        """
         self.title = title.strip()
         self.date = date.strip()
         self.time = time.strip()
@@ -15,28 +24,47 @@ class Exam:
 
     @property
     def code(self) -> int:
-        """Extracts 3 digit course code from title."""
-        match_object: None | re.Match = re.search(r"\d{3}", self.title)
+        """Extracts the 3-digit course code from the title.
+
+        Returns:
+            int: The extracted course code.
+
+        Raises:
+            ValueError: If the exam title does not contain a 3-digit course code.
+        """
+        match_object = re.search(r"\d{3}", self.title)
         if match_object is None:
             raise ValueError(
-                f"Exam title {self.title} does not contain a 3 digit course code."
+                f"Exam title {self.title} does not contain a 3-digit course code."
             )
         else:
             return int(match_object.group())
 
     @property
     def requires_phd_proctor(self) -> bool:
-        """Returns True if exam requires a PhD proctor."""
+        """Check if the exam requires a PhD proctor.
+
+        Returns:
+            bool: True if the exam requires a PhD proctor, False otherwise.
+        """
         return self.code >= 500
 
     @property
     def is_first_year_masters_exam(self) -> bool:
-        """Returns True if exam is for first year masters students."""
+        """Check if the exam is for first-year masters students.
+
+        Returns:
+            bool: True if the exam is for first-year masters students, False otherwise.
+        """
         return self.code in FIRST_YEAR_MASTERS_COURSES and "ECON" in self.title
 
     @property
     def is_second_year_masters_exam(self) -> bool:
-        """Returns True if exam is for second year masters students."""
+        """Check if the exam is for second-year masters students.
+
+        Returns:
+            bool: True if the exam is for second-year masters students, False otherwise.
+        """
         return (
             self.code >= 500
             and "ECON" in self.title
@@ -45,9 +73,18 @@ class Exam:
 
     @property
     def block(self) -> str:
-        """Returns the block of the exam."""
+        """Get the block of the exam.
+
+        Returns:
+            str: The block of the exam, represented as a combination of date and time.
+        """
         return self.date + " " + self.time
 
     def __repr__(self) -> str:
+        """Return a string representation of the Exam object.
+
+        Returns:
+            str: A string representation of the Exam object.
+        """
         return f"""EXAM: {self.title}, {self.block}, {self.classroom}, {self.instructor}
 ATTRIBUTES: 1st Year MA Exam = {self.is_first_year_masters_exam}, 2nd Year MA Exam = {self.is_second_year_masters_exam}, Requires PhD Proctor = {self.requires_phd_proctor}"""
